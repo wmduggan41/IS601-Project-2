a287e562eefa9f45a10199e02288695052fcbca6
wmd4@njit.edu <wmd4@njit.edu>
2021-07-31 14:05:16 -0400

    Feature: csv reader file

---
+++ src/Statistics/CsvReader.py
@@ -0,0 +1,12 @@
+import csv
+
+
+class CsvReader:
+    data = []
+
+    def __init__(self, filepath):
+        self.data = []  # reset file data
+        with open(filepath) as text_data:
+            csv_data = csv.DictReader(text_data, delimiter=',')
+            for row in csv_data:
+                self.data.append(row)
